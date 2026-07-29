327005e4a0855ed4aa16bdc0ed2b305d7f5168c0
justcallmelightt <utubecuro@gmail.com>
2026-07-29 19:33:06 +0900

    chore(release): prepare v2.0.0-alpha.1

--- config.public.js
+++ config.public.js
@@ -1,4 +1,7 @@
 window.TILE_NEIS_CONFIG = {
+  apiBase: window.location.hostname === "justcallmelightt.github.io"
+    ? "https://tile0.vercel.app/api/neis"
+    : "",
   schoolName: "미림마이스터고등학교",
   grade: "1",
   className: "2"
